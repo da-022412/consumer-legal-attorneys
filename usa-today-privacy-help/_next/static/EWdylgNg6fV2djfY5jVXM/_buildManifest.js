3b175a3e3872d39784b7830875c92eb052da4df2
Dennis Acosta <email@dennisacosta.me>
2022-03-10 19:47:44 -0500

    Add content

---
+++ usa-today-privacy-help/_next/static/EWdylgNg6fV2djfY5jVXM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b09c7097335b5252887a.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
